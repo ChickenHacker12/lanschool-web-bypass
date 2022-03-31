bec19bbd26788d7285fc1e372ff05ef12849872f
Trey Owen <treyeowen@gmail.com>
2022-03-31 16:27:47 -0500

    Added plugin
    Added clean-webpack-plugin to clean the build folder on build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,15 @@
 const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 });
