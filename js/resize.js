e857ce0c55c175e83f64ddd84db4abb617656e20
Trey Owen <treyeowen@gmail.com>
2022-04-01 14:21:59 -0500

    Changed spacing
    Changed comment; updated toolbar spacing

--- js/resize.js
+++ js/resize.js
@@ -2,5 +2,5 @@ import { toolbarBox, browserBox } from "./index.js";
 
 export function resizeBrowser(height) {
   browserBox.style.height = `${height - toolbarBox.getBoundingClientRect().height}px`;
-  console.log("RESIZED");
+  console.info("Window resized.");
 }
